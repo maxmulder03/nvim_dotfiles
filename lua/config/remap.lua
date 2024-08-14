16bc05a6c1494bc1c8c913c39841610756fae6a1
Max Mulder <maxmulder03@gmail.com>
2024-08-13 20:13:01 -0400

    remaps from PT

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,2 +1,33 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Move highlighted text up and down with cap j & k
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- J stays in place
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Jump up and down page with cursor in the middle
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Search terms stay in the middle 
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- ooooohhh yes paste good
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- super yank copy to outside clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- 
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- find and replace the word youre on!
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- basically chmod +x in a file
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
